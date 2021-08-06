24188ddbe6d5fc147f053bb67dad709d5b1ce945
Robert Goss <goss.robert@gmail.com>
2021-08-06 23:44:59 +0100

    Add more result levels

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod laplacian_branching;
 fn main() {
     let mut sim = laplacian_branching::LaplacianBranchingSim::init(480, 320, 10.0, 5.0, 10.0, 0.5);
     sim.image().save(format!("sim_{}.png", 0)).ok();
-    for i in 1..16 {
+    for i in 1..32 {
         sim.grow();
         println!("Stage {} - {} growing tips", i, sim.growing_tips());
         sim.image().save(format!("sim_{}.png", i)).ok();
